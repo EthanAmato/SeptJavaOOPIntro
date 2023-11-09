029e13ebba5331f7810837c70463b43ed07520b7
EthanAmato <ethannamato@gmail.com>
2023-11-09 11:56:22 -0500

    extra comments

--- src/com/tts/day3/factory/ShapeDemo.java
+++ src/com/tts/day3/factory/ShapeDemo.java
@@ -21,12 +21,24 @@ public class ShapeDemo {
 		// This means that changes to the creation logic (ShapeFactory.getShape()) or the concrete 
 		// classes (Square, Circle, Rectangle) DO NOT AFFECT client code
 		
-		ShapeFactory shapeFactory = new ShapeFactory();
+		// 3. Single Responsibility Principle -> Factories centralize the creation logic for our shape 
+		// objects, meaning that the factory class is solely responsible for the lifecycle of these objects.
 		
+		// 4. Ease of configuration -> Factories can be designed to create objects based on different parameters
+		// i.e. we could theoretically overload our getShape method to include radius or width/height.
+		
+		
+		ShapeFactory shapeFactory = new ShapeFactory();
+		 
 		Shape myFactoryMadeCircle = shapeFactory.getShape(ShapeType.CIRCLE);
 		myFactoryMadeCircle.draw();
 		
 		Shape myFactoryMadeSquare = shapeFactory.getShape(ShapeType.SQUARE);
-		myFactoryMadeSquare.draw();
+		myFactoryMadeSquare.draw();		
+		
+		// Not allowed because we didn't add TRIANGLE to our Enum - therefore
+		// users cannot even try to ask for the creation of a triangle from
+		// our shapeFactory
+		// Shape myFactoryMadeTriangle = shapeFactory.getShape(ShapeType.TRIANGLE);
 	}
 }
